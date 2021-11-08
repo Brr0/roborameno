8328744a562c4d0c58583ef5f9aa57b2843859b1
Brr0 <47314980+Brr0@users.noreply.github.com>
2021-11-08 13:53:41 +0100

    update

--- rameno/rameno.ino
+++ rameno/rameno.ino
@@ -1,28 +1,36 @@
 #include <Servo.h>
+#include <Arduino.h>
+#define POSITIONS '5'
+#define SERVOS '4'
 
-Servo servo1;
-Servo servo2;
-Servo servo3;
+Servo servo[SERVOS];
+
+int pins[4] = {2, 3, 4, 5};
+int maxposition[SERVOS][2] = {{0, 180}, {70, 110}, {120, 170}, {0, 180}};
+int position[SERVOS][POSITIONS];
 
 
-int pos1[3] = {0, 70, 120};
-int pos2[3] = {180, 110, 170}; // variable to store the servo position
 
-void setup() {
-  servo1.attach(2);
-  servo2.attach(3);
-  servo3.attach(4);// attaches the servo on pin 9 to the servo object
+void setup()
+{
+  for (int i = 0; i < SERVOS; i++)
+  {
+    servo[i].attach(pins[i]);
+  }
+  Serial.begin(9600);
 }
 
-void loop() {
-
-  servo1.write(pos1[0]);
-  servo2.write(pos1[1]);
-  servo3.write(pos1[2]);
-  delay(2000);
-  servo1.write(pos2[0]);
-  servo2.write(pos2[1]);
-  servo3.write(pos2[2]);// tell servo to go to position in variable 'pos'
-  delay(2000);                       // waits 15ms for the servo to reach the position
-
+void loop()
+{
+  for (int j = 0; j < POSITIONS; j++)
+  {
+    for (int i = 0; i < SERVOS; i++)
+    {
+      if (maxposition[i][0] < position[i][j] < maxposition[i][1])
+      {
+        servo[i].write(position[i][j]);
+      }
+    }
+  }
 }
+
