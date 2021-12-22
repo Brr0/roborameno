b7d9485cbc6d9d095d01d8822d72fa8e9f4e93d7
Brr0 <47314980+Brr0@users.noreply.github.com>
2021-12-22 18:27:04 +0100

    Final working version

--- rameno/rameno.ino
+++ rameno/rameno.ino
@@ -1,32 +1,31 @@
 #include <Servo.h>
 #include <Arduino.h>
-#define POSITIONS '5'
-#define SERVOS '4'
-
-Servo servo[SERVOS];
+#define SERVOS 4
+#define POSITIONS 8
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 int pins[4] = {2, 3, 4, 5};
-int maxposition[SERVOS][2] = {{0, 180}, {70, 110}, {120, 170}, {0, 180}};
-int position[SERVOS][POSITIONS];
+int position[SERVOS][POSITIONS] = {{45, 45, 60, 60, 90, 90, 60, 60}, {116, 116, 85, 85, 85, 85, 85, 85}, {123, 123, 150, 175, 175, 175, 175, 140}, {30, 10, 10, 10, 10, 30, 30, 30}};
 
-
-
-void setup()
-{
-  for (int i = 0; i < SERVOS; i++)
-  {
-    servo[i].attach(pins[i]);
-  }
-  Serial.begin(9600);
+void setup() {
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
 }
+void loop() {
 
-void loop()
-{
-  for (int j = 0; j < POSITIONS; j++)
-  {
-    for (int i = 0; i < SERVOS; i++)
-    {
-      servo[i].write(map(position[i][j]),0,100,maxposition[i][0],maxposition[i][1]);
-    }
+  for (int i = 0; i < POSITIONS; i++) {
+    servo1.write(position[0][i]);
+    delay(20);
+    servo2.write(position[1][i]);
+    delay(20);
+    servo3.write(position[2][i]);
+    delay(20);
+    servo4.write(position[3][i]);
+    delay(200);
   }
 }
