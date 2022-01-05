6107d50ffca16a4b106ae569d27c53b0c7b64d1e
Brr0 <47314980+Brr0@users.noreply.github.com>
2022-01-05 08:20:08 +0100

    přídání vysvětlení kodu

--- rameno/rameno.ino
+++ rameno/rameno.ino
@@ -1,23 +1,28 @@
 #include <Servo.h>
 #include <Arduino.h>
+//Počet servo motorů.
 #define SERVOS 4
+//Počet pozic, kterýma rameno projde.
 #define POSITIONS 8
+//Definování objektů typu servo.
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
 
+//Piny na které jsou serva připojeny.
 int pins[4] = {2, 3, 4, 5};
 int position[SERVOS][POSITIONS] = {{45, 45, 60, 60, 90, 90, 60, 60}, {116, 116, 85, 85, 85, 85, 85, 85}, {123, 123, 150, 175, 175, 175, 175, 140}, {30, 10, 10, 10, 10, 30, 30, 30}};
 
 void setup() {
-  servo1.attach(2);
-  servo2.attach(3);
-  servo3.attach(4);
-  servo4.attach(5);
+  //Přiřazení objektů k digitálním výstupním pinům.
+  servo1.attach(pins[0]);
+  servo2.attach(pins[1]);
+  servo3.attach(pins[2]);
+  servo4.attach(pins[3]);
 }
 void loop() {
-
+  //Cyklus for, který při každém průběhu nastaví jednotlivé hodnoty serv na hodnoty pro danou polohu ramena. Cyklus proběhne tolikrát, kolik je nastaveno v proměnné POSITIONS.
   for (int i = 0; i < POSITIONS; i++) {
     servo1.write(position[0][i]);
     delay(20);
