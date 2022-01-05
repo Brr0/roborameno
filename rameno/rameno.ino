eb7fbdec660d2e0adc9c0a52db4b5ae806c68677
Brr0 <47314980+Brr0@users.noreply.github.com>
2022-01-05 09:03:32 +0100

    Přídání systémového přerušení

--- rameno/rameno.ino
+++ rameno/rameno.ino
@@ -15,12 +15,20 @@ int pins[4] = {2, 3, 4, 5};
 int position[SERVOS][POSITIONS] = {{45, 45, 60, 60, 90, 90, 60, 60}, {116, 116, 85, 85, 85, 85, 85, 85}, {123, 123, 150, 175, 175, 175, 175, 140}, {30, 10, 10, 10, 10, 30, 30, 30}};
 
 void setup() {
+  attachInterrupt(digitalPinToInterrupt(18), sleep, LOW);
   //Přiřazení objektů k digitálním výstupním pinům.
   servo1.attach(pins[0]);
   servo2.attach(pins[1]);
   servo3.attach(pins[2]);
   servo4.attach(pins[3]);
 }
+
+//pozastavení práce ramena
+void sleep() {
+  delay(10);
+}
+
+
 void loop() {
   //Cyklus for, který při každém průběhu nastaví jednotlivé hodnoty serv na hodnoty pro danou polohu ramena. Cyklus proběhne tolikrát, kolik je nastaveno v proměnné POSITIONS.
   for (int i = 0; i < POSITIONS; i++) {
